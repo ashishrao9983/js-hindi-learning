3ce8f865587ceed0e31f30c7050363e8923b1331
ashishrao9983 <ashishrao9983@gmail.com>
2025-02-04 13:39:34 +0000

    container config done

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,16 @@
+"use strict";//use all code of javascript as newer version
+
+console.log(3+3)
+
+console.log("Ashish Rao")
+
+
+let name = "ashish"
+let age =18
+let isloggedIn= false
+
+let state;
+
+console.log(typeof "ashish")
+console.log(typeof null)
+console.log(typeof age)
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144543
+let accountEmail ="ashishrao9983@gmail.com"
+var accountPassword = "12345"
+accountCity= "Jaipur"
+let accountState ;
+// accountId = 2
+
+
+accountEmail = "ashishrao8875@gmail.com"
+accountPassword ="8875"
+accountCity ="bengolaru"
+
+
+console.table([accountId ,accountEmail,accountPassword,accountCity,accountState])

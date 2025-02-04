f83d064741eb1e692f4f2b272661a179df5b581b
ashishrao9983 <ashishrao9983@gmail.com>
2025-02-04 14:23:14 +0000

    container config done

---
+++ 01_basics/01_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "99abc"
+
+console.log(typeof score)
+
+ console.log(typeof (score))
+
+ // 33 ->number
+ // 33abc ->NaN
+ // "33"->string
+
+
+let valueInNumber =Number(score)
+ console.log(typeof valueInNumber)
+ console.log(valueInNumber)
+
+
+let isloggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isloggedIn)
+console.log(booleanIsLoggedIn)
+
+//1 ->true    0->false
+//""->false    
+//"ashish" ->true;
+
+let someNumber =33
+
+let stringNumber = String(someNumber)
+console.log( typeof stringNumber)
